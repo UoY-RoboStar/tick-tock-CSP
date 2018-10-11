9062fa78ce3694680e29dddfeb56871ee15e4f3a
James Baxter <jeb531@york.ac.uk>
2018-10-11 16:37:36 +0100

    Add CTockTick compositionality proofs

--- CTockTick.thy
+++ CTockTick.thy
@@ -2895,3 +2895,33 @@ section {* Refinement *}
 
 definition RefinesCTT :: "'e cttobs list set \<Rightarrow> 'e cttobs list set \<Rightarrow> bool" (infix "\<sqsubseteq>\<^sub>C" 50) where
   "P \<sqsubseteq>\<^sub>C Q = (Q \<subseteq> P)"
+
+subsection {* Compositionality *}
+
+lemma Prefix_compositional: 
+  "P \<sqsubseteq>\<^sub>C Q \<Longrightarrow> e \<rightarrow>\<^sub>C P \<sqsubseteq>\<^sub>C e \<rightarrow>\<^sub>C Q"
+  unfolding RefinesCTT_def PrefixCTT_def by auto
+
+lemma IntChoice_compositional1: 
+  "P \<sqsubseteq>\<^sub>C Q \<Longrightarrow> P \<sqinter>\<^sub>C R \<sqsubseteq>\<^sub>C Q \<sqinter>\<^sub>C R"
+  unfolding RefinesCTT_def IntChoiceCTT_def by auto
+
+lemma IntChoice_compositional2: 
+  "P \<sqsubseteq>\<^sub>C Q \<Longrightarrow> R \<sqinter>\<^sub>C P \<sqsubseteq>\<^sub>C R \<sqinter>\<^sub>C Q"
+  unfolding RefinesCTT_def IntChoiceCTT_def by auto
+
+lemma ExtChoice_compositional1: 
+  "P \<sqsubseteq>\<^sub>C Q \<Longrightarrow> P \<box>\<^sub>C R \<sqsubseteq>\<^sub>C Q \<box>\<^sub>C R"
+  unfolding RefinesCTT_def ExtChoiceCTT_def by auto
+
+lemma ExtChoice_compositional2: 
+  "P \<sqsubseteq>\<^sub>C Q \<Longrightarrow> R \<box>\<^sub>C P \<sqsubseteq>\<^sub>C R \<box>\<^sub>C Q"
+  unfolding RefinesCTT_def ExtChoiceCTT_def by auto
+
+lemma SeqComp_compositional1: 
+  "P \<sqsubseteq>\<^sub>C Q \<Longrightarrow> P ;\<^sub>C R \<sqsubseteq>\<^sub>C Q ;\<^sub>C R"
+  unfolding RefinesCTT_def SeqCompCTT_def by auto
+
+lemma SeqComp_compositional2: 
+  "P \<sqsubseteq>\<^sub>C Q \<Longrightarrow> R ;\<^sub>C P \<sqsubseteq>\<^sub>C R ;\<^sub>C Q"
+  unfolding RefinesCTT_def SeqCompCTT_def by auto
