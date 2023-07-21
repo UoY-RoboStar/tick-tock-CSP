3b175c7c0652b6f16138dc9da3aaaf7ad3975409
Pedro Ribeiro <pedro.ribeiro@york.ac.uk>
2023-07-21 10:40:57 +0100

    Fix output.

--- fdr/run-tests.sh
+++ fdr/run-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-output=$( refines --brief --quiet fdr/examples.csp 2>&1 | tee /dev/tty )
+output=$( refines --brief --quiet fdr/examples.csp )
+
+echo $output
 
 if echo $output | grep -q 'Failed'; then
     echo "At least one assertion failed";
